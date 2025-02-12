6919c96f28669fb7691702d54d60bac458890367
Cj Karinen <Cjkarinen20@gmail.com>
2025-02-11 21:09:56 -0500

    Collision

--- tetromino.py
+++ tetromino.py
@@ -8,30 +8,60 @@ class Block(pg.sprite.Sprite):
     def __init__ (self, tetromino, pos):
         self.tetromino = tetromino
         self.pos = vec(pos) + INIT_POS_OFFSET
+        self.alive = True
         
         super().__init__(tetromino.pytris.sprite_group)
         self.image = pg.Surface((TILE_SIZE, TILE_SIZE))
-        self.image.fill('orange')
+        pg.draw.rect(self.image, 'red', (1, 1, TILE_SIZE - 2, TILE_SIZE - 2), border_radius = 8)
         
         self.rect = self.image.get_rect()
         self.rect.topleft = self.pos * TILE_SIZE
+    
+    def rotate(self, pivot_pos):
+        translated = self.pos - pivot_pos
+        rotated = translated.rotate(90)
+        return rotated + pivot_pos
     
     def set_rect_pos(self):
         self.rect.topleft = self.pos * TILE_SIZE
     
     def update(self):
         self.set_rect_pos()
+        
+    def has_collision(self, pos):
+        x, y = int(pos.x), int(pos.y)
+        if 0 <= x < FIELD_W and 0 <= y < FIELD_H and (
+            y < 0 or not self.tetromino.pytris.field_array[y][x]):
+            return False
+        return True
 
 class Tetromino:
     def __init__(self, pytris):
         self.pytris = pytris
         self.shape = random.choice(list(TETROMINOES.keys()))
         self.blocks = [Block(self, pos) for pos in TETROMINOES[self.shape]]
+        self.landing = False
+        
+    def rotate(self):
+        pivot_pos = self.blocks[0].pos
+        new_block_positions = [block.rotate(pivot_pos) for block in self.blocks]
+        
+        if not self.has_collision(new_block_positions):
+            for i, block in enumerate(self.blocks):
+                block.pos = new_block_positions[i]
+       
+    def has_collision(self, block_positions):
+        return any(map(Block.has_collision, self.blocks, block_positions))
         
     def move (self, direction):
         move_direction = MOVE_DIRECTIONS[direction]
-        for block in self.blocks:
-            block.pos += move_direction
-        
+        new_block_positions = [block.pos + move_direction for block in self.blocks]
+        has_collide = self.has_collision(new_block_positions)
+        if not has_collide:
+            for block in self.blocks:
+                block.pos += move_direction
+        elif direction == 'down':
+            self.landing = True
+            
     def update (self):
         self.move(direction = 'down')
